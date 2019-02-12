db6663c298309692b59cd7bb860dc7221ba6e779
DESKTOP-18ROEJ5\zxq <1044887175@qq.com>
2019-02-12 16:33:28 +0800

    add 初始化

---
+++ CostDetail/CostRecordPlan/CostRecord/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CostRecord
+{
+    class Program
+    {
+        static int publicFunds = 0;
+        static void Main(string[] args)
+        {
+            //zxq
+            publicFunds += 0;
+
+
+
+            System.Console.WriteLine("11");
+            Console.ReadLine();
+        }
+    }
+}
